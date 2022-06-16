6e7b1063e3359969a0b684bd1201a2243e35afc1
Raj Belani <107609015+rajbelani@users.noreply.github.com>
2022-06-16 13:00:33 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,6 @@ import streamlit
 streamlit.title('My Parents New Healthy Dinner')
 
 streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 and Blueberry Oatmeal')
-streamlit.text('kale,spinach & Rocket Smoothie')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
