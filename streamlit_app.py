e48f5007f8c3adcfd3fd8ce7061dbd909a7d995a
Raj Belani <107609015+rajbelani@users.noreply.github.com>
2022-06-17 19:52:13 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -50,6 +50,6 @@ streamlit.dataframe(my_data_rows)
 
 def insert_row_snowflake(new_fruit):
 with my_cnx.cursor() as my_cur:
-my_cur.execute("insert into fruit_load_list values ('" +"apple"+"')")
+my_cnx.execute("insert into fruit_load_list values ('" +"apple"+"')")
 return "Thanks for adding" + new_fruit
 
